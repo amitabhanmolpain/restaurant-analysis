ef3531582e34154d63725e072cbc29f44ec965fe
Amitabh Anmol Pain <ami05tabh@gmail.com>
2025-05-08 23:24:53 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import brand_img from './brand_img.png'
+import Headerr from './Headerr.png'
+import H4 from './H4.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+    Headerr,
+    H4,
+}
+
+
+  
