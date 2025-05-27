a33021bcde294433308ad24aaf4e5472d7acf506
Amitabh Anmol Pain <ami05tabh@gmail.com>
2025-05-28 00:32:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
+import Middle from './components/Middle';
 import Dashboard from './components/Dashboard';
 import MenuManager from './components/MenuManager';
+import Features from './components/Features';
 
 const App = () => {
   return (
-    <div className="w-full overflow-hidden">
+    <div className="w-full min-h-screen"> {/* Removed overflow-hidden, added min-h-screen */}
       <Router>
         <Routes>
           <Route path="/" element={<About />} />
+          <Route path="/middle" element={<Middle />} /> {/* Added route for Middle */}
+          <Route path="/Features" element={<Features />} /> {/* Added route for Middle */}
+          
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/menu" element={<MenuManager />} />
         </Routes>
